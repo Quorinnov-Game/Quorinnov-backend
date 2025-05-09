e0a51fdcdb28135ccb6e22d591fe99611d080ab7
lewan <quangle1113@gmail.com>
2025-05-10 00:12:05 +0200

    update player

--- models/player.py
+++ models/player.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
+from sqlalchemy.dialects.postgresql import JSON
 
 class Player(Base):
     __tablename__ = "players"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    position = Column(String, nullable=False)  # ví dụ "e1"
-    direction = Column(String, nullable=False)  # "up" hoặc "down"
+    color = Column(String, nullable=False)  # ví dụ "red" or "blue"
+    name = Column(String, nullable=True)
+    position = Column(JSON, nullable=False)  # ví dụ "e1"
+    direction = Column(String, nullable=True)  # "up" hoặc "down"
     walls_left = Column(Integer, default=10)
